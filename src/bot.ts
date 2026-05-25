8739f9750361798b433976cd9bc45420c69aa622
ChrisOguji <102419029+ChrisOguji@users.noreply.github.com>
2026-05-25 06:20:36 +0100

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,13 @@
+import * as http from 'http';
+
+const PORT = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is healthy');
+}).listen(PORT, '0.0.0.0', () => {
+  console.log(`🤖 Server listening on port ${PORT}`);
+});
+
 import axios from 'axios';
 import { Pool } from 'pg';
 import { Telegraf } from 'telegraf';
